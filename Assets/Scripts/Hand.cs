40af6f491d72fbfb134f901e3affc2ee4ec26c4a
Jacob-T-Gardner <julzhu27@bergen.org>
2024-12-11 19:18:37 -0500

    groundwork for hand
    im hungry

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections.Generic;
+public class Hand : MonoBehaviour
+{
+    List<Card> hand;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        hand = new List<Card>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddCard(Card card)
+    {
+        hand.Add(card);
+        hand.Sort(delegate(Card x, Card y)
+        {
+            return x.cardNumber - y.cardNumber;
+        });
+        
+        
+    }
+
+    
+}
