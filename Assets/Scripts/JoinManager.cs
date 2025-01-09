986832bb06e1e080248ddaf21cfca8ffa3fbe85c
Pinak Unawane <pinuna27@bergen.org>
2025-01-09 15:41:24 -0500

    overriding port logic

--- Assets/Scripts/JoinManager.cs
+++ Assets/Scripts/JoinManager.cs
@@ -1,15 +1,20 @@
 using System;
 using Unity.Netcode;
 using UnityEngine;
+using Unity.Netcode.Transports.UTP;
 using Screen = UnityEngine.Device.Screen;
 
 public class JoinGame : MonoBehaviour
 {
     private NetworkManager _networkManager;
-
+    private ushort port;
+    
     private void Awake()
     {
         _networkManager = GetComponent<NetworkManager>();
+
+        // Default port is 7777
+        port = 12345;
     }
 
     private void OnGUI()
@@ -28,15 +33,22 @@ public class JoinGame : MonoBehaviour
         {
             if (GUILayout.Button("Join Game!", style))
             {
+                UpdatePort(port);
                 _networkManager.StartClient();
             }
             // TODO: DELETE THIS IN PRODUCTION 🙏
             if (GUILayout.Button("Server"))
-            {
+            {   
+                UpdatePort(port);
                 _networkManager.StartServer();
             }
         }
         GUILayout.EndArea();
+    }
+
+    private void UpdatePort(ushort port) {
+        var unityTransport = (UnityTransport) _networkManager.NetworkConfig.NetworkTransport;
+        unityTransport.ConnectionData.Port = port;
     }
 
     void Start()
