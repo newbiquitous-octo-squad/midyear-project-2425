b8c55a955f9fd80a78cda7e9011ce305d3f7f246
Pinak Unawane <pinuna27@bergen.org>
2025-01-09 23:05:05 -0500

    boilerplate code to update port/ip in networkManager

--- Assets/Scripts/JoinManager.cs
+++ Assets/Scripts/JoinManager.cs
@@ -8,56 +8,58 @@ public class JoinGame : MonoBehaviour
 {
     private NetworkManager _networkManager;
     private ushort port;
-    
+    private string ipAddress;
+
     private void Awake()
     {
         _networkManager = GetComponent<NetworkManager>();
 
         // Default port is 7777
         port = 12345;
+        // Default IP address is 127.0.0.1
+        ipAddress = "127.0.0.1";
     }
 
     private void OnGUI()
     {
         GUI.color = Color.white;
-        GUILayout.BeginArea(new Rect(Screen.width/2 - 200, Screen.height / 1.5f,400,400));
+        GUILayout.BeginArea(new Rect(Screen.width / 2 - 200, Screen.height / 1.5f, 400, 400));
         var style = new GUIStyle();
         style.fontSize = 60;
-        style.font = Resources.Load<Font>("Fonts/casino.3d-marquee-regular");        style.alignment = TextAnchor.MiddleCenter;
+        style.font = Resources.Load<Font>("Fonts/casino.3d-marquee-regular"); style.alignment = TextAnchor.MiddleCenter;
         style.normal.background = Texture2D.grayTexture;
         style.normal.textColor = Color.white;
         style.active.background = Texture2D.whiteTexture;
-        
-        
+
         if (!_networkManager.IsClient && !_networkManager.IsServer)
         {
             if (GUILayout.Button("Join Game!", style))
             {
-                UpdatePort(port);
+                UpdateConnectionData(ipAddress, port);
                 _networkManager.StartClient();
             }
             // TODO: DELETE THIS IN PRODUCTION 🙏
             if (GUILayout.Button("Server"))
-            {   
-                UpdatePort(port);
+            {
+                UpdateConnectionData(ipAddress, port);
                 _networkManager.StartServer();
             }
         }
         GUILayout.EndArea();
     }
 
-    private void UpdatePort(ushort port) {
-        var unityTransport = (UnityTransport) _networkManager.NetworkConfig.NetworkTransport;
-        unityTransport.ConnectionData.Port = port;
+    private void UpdateConnectionData(string ipAddress, ushort port)
+    {
+        ((UnityTransport) _networkManager.NetworkConfig.NetworkTransport).SetConnectionData(ipAddress, port);
     }
 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        
+
     }
 }
