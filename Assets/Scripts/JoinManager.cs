33b6d9c8b2a2acff5963b289b4f3184e121e8f48
MiklPolygon <154548611+MiklPolygon@users.noreply.github.com>
2025-01-11 18:34:13 -0500

    added panel for lobby system
    no real functionality yet, but you can open and close the panel!!!

--- Assets/Scripts/JoinManager.cs
+++ Assets/Scripts/JoinManager.cs
@@ -9,6 +9,7 @@ public class JoinGame : MonoBehaviour
     private ushort port;
     private string ipAddress;
     private string serverListenAddress;
+    private bool showPanel;
 
     private void Awake()
     {
@@ -28,7 +29,8 @@ public class JoinGame : MonoBehaviour
         GUILayout.BeginArea(new Rect(Screen.width / 2f - 200, Screen.height / 1.5f, 400, 400));
         var style = new GUIStyle();
         style.fontSize = 60;
-        style.font = Resources.Load<Font>("Fonts/casino.3d-marquee-regular"); style.alignment = TextAnchor.MiddleCenter;
+        style.font = Resources.Load<Font>("Fonts/casino.3d-marquee-regular");
+        style.alignment = TextAnchor.MiddleCenter;
         style.normal.background = Texture2D.grayTexture;
         style.normal.textColor = Color.white;
         style.active.background = Texture2D.whiteTexture;
@@ -46,12 +48,44 @@ public class JoinGame : MonoBehaviour
                 UpdateConnectionData(ipAddress, port, serverListenAddress);
                 _networkManager.StartServer();
             }
+            if (GUILayout.Button("Show Panel"))
+            {
+                showPanel = true;
+            }
         }
         GUILayout.EndArea();
+
+        if (showPanel)
+        {
+            float panelWidth = Screen.width * 0.7f;
+            float panelHeight = Screen.height * 0.8f;
+            float panelX = (Screen.width - panelWidth) / 2;
+            float panelY = (Screen.height - panelHeight) / 2;
+
+            GUI.color = Color.gray;
+            GUI.DrawTexture(new Rect(panelX, panelY, panelWidth, panelHeight), Texture2D.whiteTexture);
+            GUI.color = Color.white;
+
+            var textStyle = new GUIStyle();
+            textStyle.fontSize = 20;
+            textStyle.font = Resources.Load<Font>("Fonts/casino.3d-marquee-regular");
+            textStyle.alignment = TextAnchor.MiddleCenter;
+            textStyle.normal.textColor = Color.cyan;
+
+            GUILayout.BeginArea(new Rect(panelX, panelY, panelWidth, panelHeight));
+            GUILayout.Label("Lobby joining system to be implemented here:", textStyle);
+            GUILayout.Label("Format & do the client (maybe host) handling within this panel", textStyle);
+            GUILayout.EndArea();
+
+            if (GUI.Button(new Rect(panelX + 25, panelY + 25, 75, 75), "X", style))
+            {
+                showPanel = false;
+            }
+        }
     }
 
     private void UpdateConnectionData(string ipAddress, ushort port, string serverListenAddress)
     {
-        ((UnityTransport) _networkManager.NetworkConfig.NetworkTransport).SetConnectionData(ipAddress, port, serverListenAddress);
+        ((UnityTransport)_networkManager.NetworkConfig.NetworkTransport).SetConnectionData(ipAddress, port, serverListenAddress);
     }
 }
